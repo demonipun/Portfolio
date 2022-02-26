025829a7fa160c0cd3023b84e898bf06bbff9af3
demonipun <71482945+demonipun@users.noreply.github.com>
2022-02-26 22:48:21 +0530

    Styled Navigation Bar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';          // useState import kiya for  setToggle
+
+import { HiX, HiMenuAlt4 } from 'react-icons/hi'; // import icons
+import { motion } from 'framer-motion';           // Framer-Motion
 
 import { images } from '../../constants';
 import './Navbar.scss';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 const Navbar = () => {
+  // creating setToggle
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -17,6 +25,25 @@ const Navbar = () => {
         </li>
         ))}
       </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        { toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut'}}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {['home', 'about', 'work', 'contact'].map((item) => (   // Cut and Paste part from above
+            <li key={item}>
+              <a href={`#${item}`} onClick={() => setToggle(false) }> {item} </a>
+            </li>
+            ))}
+
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
